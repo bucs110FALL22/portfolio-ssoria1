e3f22d3d27cddc8575d7866e60214e31ddc66a7f
Sergio Soria <112532072+ssoria1@users.noreply.github.com>
2022-10-04 00:01:34 +0000

    Working with while loops, it has to perform more actions.

---
+++ ch04/exercises/coinflip.py
@@ -0,0 +1,46 @@
+import turtle
+import random
+
+heads = 0 
+tails = 0
+
+window = turtle.Screen() 
+window.bgcolor('blue')
+Tito = turtle.Turtle() 
+Tito.color('green')
+Tito.shape('turtle')
+
+Tito.setpos(0,0)
+
+coin_flip = random.choices(['Heads', 'Tails'], [5, 5])[0]
+flip = print(coin_flip)
+
+
+
+flip = 1
+while flip != coin_flip:
+  #Probability_coin = coin_flip
+  if coin_flip == 'Heads':
+    Tito.left(90)
+    Tito.forward(50)
+    Tito.pendown()
+    break
+    flip = coin_flip + 1
+    
+   
+  elif coin_flip == 'Tails': 
+    Tito.right(90)
+    Tito.forward(50)
+    Tito.pendown()
+    break
+    flip = coin_flip + 1
+    
+    
+
+
+  
+
+
+
+
+window.exitonclick()
