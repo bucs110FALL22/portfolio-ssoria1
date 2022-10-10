f1c2c684720cfd5681072473431bb686b0d41731
Sergio Soria <112532072+ssoria1@users.noreply.github.com>
2022-10-10 23:53:45 +0000

    stars exercise

---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,28 @@
+#
+#
+#
+def star_pyramid():
+ stars = int(input("Introduce your number of rows: "))
+ count = 0
+ while count <= stars:
+   print('*'*count)
+   count = count + 1     
+star_pyramid()
+
+
+
+
+
+
+def rstar_pyramid():
+ stars = int(input("Introduce your number of rows: "))
+ count = stars
+ while count >= 0:
+   print('*'*count)
+   count = count -1     
+rstar_pyramid()
+
+
+
+
+
