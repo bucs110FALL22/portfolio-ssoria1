9688edbdb0ae8ee4ac2e6dce875259751c14ee07
Sergio Soria <112532072+ssoria1@users.noreply.github.com>
2022-10-20 03:41:27 +0000

    scope

---
+++ ch05/exercises/scope.py
@@ -0,0 +1,36 @@
+#
+#
+#
+#
+#
+#
+def first_function(x, y):
+  count = 0
+  for i in range(1, second + 1):
+   count = count + first
+  return count
+
+first = int(input("Introduce your first number: "))
+second = int(input("Introduce your second number:  "))
+print(first_function(first, second))
+#
+#
+#
+def second_function(x, y):
+  Result = 1 
+  for i in range(exponent):
+    Result *= base_number
+  return Result
+
+base_number = int(input("Introduce your primary number: "))
+exponent = int(input("Introduce a number as an exponent:  ")) 
+print(second_function(base_number, exponent)) 
+#
+#
+#
+#
+#
+#
+def square():
+  print(second_function(7, 2))
+square()
