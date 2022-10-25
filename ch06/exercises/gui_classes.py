0ae5f9ca2527141035cfe045909eed1e5798c156
Sergio Soria <112532072+ssoria1@users.noreply.github.com>
2022-10-25 00:07:42 +0000

    classes chapter 06

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,26 @@
+#
+#
+#
+class Tubes:
+  def __init__(structure, color, direction, inactive):
+    structure.color = "green" 
+    structure.direction = True
+    structure.inactive = False
+#
+#
+#
+class Enemies:
+  def __init__(goombas, color, motion, danger):
+    goombas.color = "brown"
+    goombas.motion = True
+    goomabs.position = (20,9)      #
+    
+  
+#
+#
+#
+class Blocks:
+  def __init__(powerup, kinds_sorprese, amount, coins):
+    powerup.kinds_sorprese = True
+    powerup.amount = 1   # 
+    powerup.coins = 5    # 
